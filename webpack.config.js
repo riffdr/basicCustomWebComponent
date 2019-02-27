e2e8968dc305fc0dd1bdecc43a5cb716f2b64e56
riffdr <mz80@kentforlife.net>
2019-02-27 10:19:48 +0100

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+   module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["babel-preset-env"],
+                 plugins: ['transform-custom-element-classes', 'transform-es2015-classes']
+          }
+        }
+      }
+    ]
+  }
+};
